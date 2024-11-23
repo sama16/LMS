9624d2e89ab354f601d1b47c9507eec05e6c7c7a
MalakSoliman29 <malaktsoliman@aucegypt.edu>
2024-11-23 21:32:58 +0200

    Update userpanel.h

--- userpanel.h
+++ userpanel.h
@@ -2,6 +2,12 @@
 #define USERPANEL_H
 
 #include <QWidget>
+#include <QComboBox>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QLabel>
 
 class UserPanel : public QWidget {
     Q_OBJECT
@@ -10,15 +16,17 @@ public:
     explicit UserPanel(QWidget *parent = nullptr);
 
 signals:
-    void loginRequested();
-    void registerRequested();
+    void loginRequested(QString username, QString password);
+    void registerRequested(QString username, QString password, QString confirmPassword);
 
 private slots:
-    void handleLoginClicked();
-    void handleRegisterClicked();
+    void handleButtonClicked();
 
 private:
-         // Add UI elements here, e.g., buttons
+    QComboBox *comboBox;
+    QLineEdit *usernameEdit, *passwordEdit, *confirmPasswordEdit;
+    QPushButton *button;
+    QLabel *usernameLabel, *passwordLabel, *confirmPasswordLabel;
 };
 
 #endif // USERPANEL_H
